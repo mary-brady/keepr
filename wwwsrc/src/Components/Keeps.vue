8bde4ac88a11b1bfb545ebf7bdbb39a68649d4e8
Mary Brady <emceebrady@gmail.com>
2018-10-15 09:10:31 -0600

    smol changes

--- wwwsrc/src/Components/Keeps.vue
+++ wwwsrc/src/Components/Keeps.vue
@@ -59,12 +59,16 @@
       <div class="modal-body">
         <form>
           <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Name:</label>
+            <label for="name-name" class="col-form-label">Name:</label>
             <input type="text" class="form-control" v-model="keepUpdate.name">
           </div>
           <div class="form-group">
-            <label for="message-text" class="col-form-label">Description:</label>
+            <label for="desc-text" class="col-form-label">Description:</label>
             <textarea class="form-control" v-model="keepUpdate.description"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="img-text" class="col-form-label">Img Url:</label>
+            <input class="form-control" v-model="keepUpdate.img">
           </div>
           <div class="form-check">
             <label class="form-check-label">
